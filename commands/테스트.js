dfc0dc311a05dcb806ea3d23d4d1984b0628c587
1teric <jinwook070806@gmail.com>
2025-11-08 02:45:36 +0900

    Update 테스트.js

--- commands/테스트.js
+++ commands/테스트.js
@@ -7,9 +7,11 @@ module.exports = {
 		.setDescription('버튼 테스트 입니다!'),
 	async execute(interaction) {
         const embed = new MessageEmbed()
-			.setColor(a6e9dfff)
-			.setTitle('테스트!')
-			.setDescription('야호');
-		await interaction.reply({embeds: [embed]});
+			.setColor('#0099ff')
+			.setTitle('Some title')
+			.setURL('https://discord.js.org')
+			.setDescription('Some description here');
+
+		await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
 	},
 };
