9d7372bbff563873e223b8784919b4ba15c8796b
1teric <157183902+1teric@users.noreply.github.com>
2025-11-07 21:03:48 +0900

    테스트.js 업데이트

--- commands/테스트.js
+++ commands/테스트.js
@@ -5,17 +5,10 @@ module.exports = {
 		.setName('테스트')
 		.setDescription('버튼 테스트 입니다!'),
 	async execute(interaction) {
-        const test = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('test')
-                    .setLabel('test')
-                    .setStyle('PRIMARY'),
-            );
         const embed = new MessageEmbed()
 			.setColor('#a6e9dfff')
 			.setTitle('테스트!')
 			.setDescription('야호');
-		await interaction.reply({content: '', ephemeral: true, embeds: [embed], components: [test]});
+		await interaction.reply({content: '', ephemeral: true, embeds: [embed]});
 	},
 };
