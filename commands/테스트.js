77bcc4ed9ee63886a60a673dbc71a59b41ad5145
1teric <jinwook070806@gmail.com>
2025-11-08 00:19:59 +0900

    .

--- commands/테스트.js
+++ commands/테스트.js
@@ -7,7 +7,7 @@ module.exports = {
 		.setDescription('버튼 테스트 입니다!'),
 	async execute(interaction) {
         const embed = new EmbedBuilder()
-			.setColor('#a6e9dfff')
+			.setColor(a6e9dfff)
 			.setTitle('테스트!')
 			.setDescription('야호');
 		await interaction.reply({embeds: [embed]});
